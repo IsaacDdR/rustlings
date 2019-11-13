dd5eda59e234166f2b759410ede289e2518bb739
IsaacDdR <isaacovic00@gmail.com>
2019-11-13 11:35:43 -0600

    Structs, Done!

--- exercises/test1.rs
+++ exercises/test1.rs
@@ -6,11 +6,15 @@
 // Mary is buying apples. One apple usually costs 2 dollars, but if you buy
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
-
-// I AM NOT DONE
-
-// Put your function here!
-// fn ..... {
+fn calculate_apple_price(apples: i8) -> i8{
+        
+    if apples > 40 {
+        apples * 1
+    } else {
+        apples * 2
+    }
+    
+}
 
 // Don't modify this function!
 #[test]
@@ -21,3 +25,5 @@ fn verify_test() {
     assert_eq!(70, price1);
     assert_eq!(65, price2);
 }
+
+
